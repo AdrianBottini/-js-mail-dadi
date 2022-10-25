85a36ebf5f91d37da9f94485ca7514ce6b092206
AdrianBottini <adrianboh@gmail.com>
2022-10-25 17:24:33 +0200

    dadi

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ const mailUtente = prompt("Qual'è la tua mail");
 const listMail = ["adrianboh@gmail.com", "giovanni90@gmail.com", "mirco83@gmail.com", "marco67@gmail.com", "michele89@gmail.com", "giovanni55@gmail.com", "matteo99@gmail.com", "paolo55@gmail.com"];
 
 for (let i = 0; i < listMail.length; i++) {
-    if ((mailUtente.indexOf("@") === -1) || (mailUtente.indexOf(".") === -1 )) {
+    if ((mailUtente.indexOf('@') === -1) || (mailUtente.indexOf(".") === -1)) {
         console.log ('devi inserire una mail valida');
     }
     else {
